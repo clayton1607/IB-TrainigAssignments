04bb9b57abb26e54b134a80e7a79be351758e7da
Clayton <claytonpereira1998@gmail.com>
2020-01-07 13:34:25 +0530

    Sql

---
+++ IBassign.sql
@@ -0,0 +1,48 @@
+create table customer(
+ custID INT primary key,
+ custName Varchar(20) not null
+ );
+ drop table customer;
+ drop table orders;
+ create table orders(
+ custID INT ,
+ orderID INT primary key,
+ orderDate date,
+ orderQty Int,
+ foreign key(custID) REFERENCES customer(custID)
+ )
+
+insert into customer(custID,custName) Values(101,'Cat'); 
+insert into customer(custID,custName) Values(102,'Dog');
+insert into customer(custID,custName) Values(103,'Fish');
+insert into customer(custID,custName) Values(104,'Rat');
+insert into customer(custID,custName) Values(105,'Tiger');
+insert into customer(custID,custName) Values(106,'Lion');
+insert into customer(custID,custName) Values(107,'Elephant');
+
+insert into orders(custID,orderID,orderDate,orderQty) Values(101,10,'2020-01-01',15);
+insert into orders(custID,orderID,orderDate,orderQty) Values(101,11,'2020-01-01',17);
+insert into orders(custID,orderID,orderDate,orderQty) Values(101,12,'2020-01-01',20);
+insert into orders(custID,orderID,orderDate,orderQty) Values(101,13,'2020-01-01',08);
+insert into orders(custID,orderID,orderDate,orderQty) Values(102,15,'2020-01-01',08);
+insert into orders(custID,orderID,orderDate,orderQty) Values(102,16,'2020-01-01',13);
+insert into orders(custID,orderID,orderDate,orderQty) Values(103,17,'2020-01-02',15);
+insert into orders(custID,orderID,orderDate,orderQty) Values(103,18,'2020-01-02',63);
+insert into orders(custID,orderID,orderDate,orderQty) Values(104,19,'2020-01-02',22);
+insert into orders(custID,orderID,orderDate,orderQty) Values(105,20,'2020-01-02',22);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,21,'2020-01-02',14);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,22,'2020-01-03',12);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,23,'2020-01-03',03);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,24,'2020-01-03',26);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,25,'2020-01-03',14);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,26,'2020-01-03',16);
+insert into orders(custID,orderID,orderDate,orderQty) Values(107,27,'2020-01-03',23);
+
+CREATE   PROCEDURE insertCUST (@xcustID  INT In ,  @xcustName  Varchar(20) in)
+AS
+begin
+	insert into customer(custID,custName) Values(@xcustID,@xcustName)
+end;
+  
+)
+select customer.custName,customer.custID,orders.orderDate,sum(orders.orderQty) from customer inner join orders on customer.custID=orders.custID group by customer.custID,orders.orderDate having sum(orders.orderQty)>50
